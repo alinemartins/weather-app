426da49db53d57ddfce5ea90a7fca17067954a26
aline-martins <aline.malonso@gmail.com>
2020-09-25 14:17:01 +1000

    Small fixes

--- js/index.js
+++ js/index.js
@@ -18,27 +18,49 @@ function getWeatherData() {
                 cityName: data.name,
                 country: data.sys.country,
                 description: data.weather[0].description,
-                feelsLike: data.main.feels_like,
-                temp: data.main.temp,
-                tempMax: data.main.temp_max,
-                tempMin: data.main.temp_min
+                feelsLike: data.main.feels_like.toFixed(1),
+                temp: data.main.temp.toFixed(1),
+                tempMax: data.main.temp_max.toFixed(1),
+                tempMin: data.main.temp_min.toFixed(1),
+                emojis: emojis[data.weather[0].icon]
             };
-            console.log(data);
             document.getElementById('cityData').innerHTML = `
             <div class="card">
                 <div class="card-body border border-info">
-                    <h5 class="card-title">${weatherData.cityName} <sup class="smaller">${weatherData.country}</sup></h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${weatherData.temp}<sup>°C</sup>, feels like ${weatherData.feelsLike}<sup>°C</sup></h6><br>
-                    <p class="card-text">${weatherData.description}</p>
+                    <h5 class="card-subtitle mb-5">${weatherData.cityName} <sup class="smaller">${weatherData.country}</sup></h5>
+                    <h6 class="card-title mb-2">${weatherData.temp}<sup>°C</sup><span class="smaller">feels like ${weatherData.feelsLike}<sup>°C</sup></span></h6><br>
+                    <p class="card-text">${weatherData.description} ${weatherData.emojis}</p>
                     <p>Min ${weatherData.tempMin}<sup>°C</sup> | Max ${weatherData.tempMax}<sup>°C</sup></p>
                 </div>
             </div>`
             ;
+
+            document.getElementById('form').reset();
         })
         .catch(function (error) {
             console.log(error);
         });
     
-    document.getElementById('form').reset();
     
 }
+
+const emojis = {
+    '01d': '☀️',
+    '02d': '⛅️',
+    '03d': '☁️',
+    '04d': '☁️',
+    '09d': '🌧',
+    '10d': '🌦',
+    '11d': '⛈',
+    '13d': '❄️',
+    '50d': '💨',
+    '01n': '☀️',
+    '02n': '⛅️',
+    '03n': '☁️',
+    '04n': '☁️',
+    '09n': '🌧',
+    '10n': '🌦',
+    '11n': '⛈',
+    '13n': '❄️',
+    '50n': '💨',
+  };
